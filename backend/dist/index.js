67ec4133b1c98646e31bad5ecfbfed908239821a
Sumith Shetty <sumithshetty014@gmail.com>
2025-01-20 00:27:55 +0530

    Created the MONGODB_URL

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { config } from "dotenv";
+config();
 const app = express();
-app.get("/hello", (req, res, next) => {
-    return res.send("Hello");
-});
+// Middlewares
+app.use(express.json());
+// Connections and Listeners
 app.listen(5000, () => console.log("Server Open"));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
-app.get("/hello", (req, res, next) => {
-    return res.send("Hello");
-})
+// Middlewares
+app.use(express.json());
 
+
+// Connections and Listeners
 app.listen(5000, ()=> console.log("Server Open"));
