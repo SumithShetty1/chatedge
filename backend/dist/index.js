8c8a7ac68d91b7857c48d59ac997cd89cbe537dd
Sumith Shetty <sumithshetty014@gmail.com>
2025-02-08 01:10:29 +0530

    Established Database Connection, Integrated OpenAI API and Created Routes to User and Chats

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,10 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-// Middlewares
-app.use(express.json());
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 // Connections and Listeners
-app.listen(5000, () => console.log("Server Open"));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected To Database"));
+})
+    .catch(err => console.log(err));
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
-import express from "express";
-import { config } from "dotenv";
-
-config();
-
-const app = express();
-
-// Middlewares
-app.use(express.json());
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 
 
 // Connections and Listeners
-app.listen(5000, ()=> console.log("Server Open"));
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase()
+    .then(() => {
+        app.listen(PORT, ()=> 
+            console.log("Server Open & Connected To Database")
+        );
+    })  
+    .catch(err=>console.log(err));
