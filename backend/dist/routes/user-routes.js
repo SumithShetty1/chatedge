dbbebb4b3b50ace01802366bc7e09a0a8fab56db
Sumith Shetty <sumithshetty014@gmail.com>
2025-02-15 20:08:30 +0530

    Created Login and Signup Feature in Backend and Created Frontend using Vite build tool and React template

--- backend/dist/routes/user-routes.js
+++ backend/dist/routes/user-routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
+import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
-userRoutes.get("/signup", userSignup);
+userRoutes.get("/signup", validate(signupValidator), userSignup);
+userRoutes.get("/login", validate(loginValidator), userLogin);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
